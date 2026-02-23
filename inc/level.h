1a16b599d40b4ab3b17b05ee50762be5b724e2a5
Dmytro <Dmytro.Burak@cs.khpi.edu.ua>
2026-02-23 17:31:32 +0200

    Deleted comments and extra {}

--- inc/level.h
+++ inc/level.h
@@ -3,15 +3,16 @@
 
 #include <SDL2/SDL.h>
 
-// Розміри тайл-мапи
 #define LEVEL_ROWS 24
 #define LEVEL_COLS 40
 #define TILE_SIZE 32
 
 typedef struct {
+
     int map[LEVEL_ROWS][LEVEL_COLS];
     float spawnX;
     float spawnY;
+    
 } Level;
 
 void loadLevelFromFile(Level *level, const char *filename);
