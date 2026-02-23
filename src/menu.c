c9a04275d8e9bee1840e8ac08431e1ff4b468295
roman <roman@ubuntu.myguest.virtualbox.org>
2026-02-23 23:44:59 +0200

    fixes: fix buttons size

--- src/menu.c
+++ src/menu.c
@@ -14,36 +14,35 @@ void updateMenu(App *app, Player *player) {
     Uint32 mouseState = SDL_GetMouseState(&mx, &my);
     bool isLeftClicked = (mouseState & SDL_BUTTON(SDL_BUTTON_LEFT));
 
-    if (!isLeftClicked)
+    if (!isLeftClicked) 
         app->mouseReleased = true;
-
-    int btnW = 300;
-    int btnH = 90;
-    int centerX = (WINDOW_WIDTH - btnW) / 2;
-
-    SDL_Rect btnPlay = { centerX, 250, btnW, btnH };
-    SDL_Rect btnSettings = { centerX, 370, btnW, btnH };
-    SDL_Rect btnExit = { centerX, 490, btnW, btnH };
+    
+    SDL_Rect btnPlay     = { 1280/2 - 100, 250, 200, 60 };
+    SDL_Rect btnSettings = { 1280/2 - 100, 350, 200, 60 };
+    SDL_Rect btnExit     = { 1280/2 - 100, 450, 200, 60 };
 
     if (isLeftClicked && app->mouseReleased) {
         if (isMouseInside(mx, my, btnPlay)) {
             Mix_HaltMusic();
-            app->state = STATE_PLAY;
+            app->state = STATE_PLAY; 
 
             app->state = STATE_PLAY;
             app->currentLevel = 1;
             app->deathCount = 0;
             player->coins = 0;
             app->gameStartTime = SDL_GetTicks();
-            if (app->level1Music)
+            
+            if (app->level1Music) 
                 Mix_PlayMusic(app->level1Music, -1);
+
         }
+        
         else if (isMouseInside(mx, my, btnSettings)) {
             app->mouseReleased = false;
             app->state = STATE_SETTINGS;
         }
-        else if (isMouseInside(mx, my, btnExit))
-            app->isRunning = false;
+        else if (isMouseInside(mx, my, btnExit)) 
+            app->isRunning = false; 
     }
 }
 
@@ -58,9 +57,9 @@ void renderMenu(App *app) {
     int mx, my;
     SDL_GetMouseState(&mx, &my);
 
-    SDL_Rect btnPlay = { 1280/2 - 100, 250, 200, 60 };
+    SDL_Rect btnPlay     = { 1280/2 - 100, 250, 200, 60 };
     SDL_Rect btnSettings = { 1280/2 - 100, 350, 200, 60 };
-    SDL_Rect btnExit = { 1280/2 - 100, 450, 200, 60 };
+    SDL_Rect btnExit     = { 1280/2 - 100, 450, 200, 60 };
 
     if (app->playButton) {
         if (isMouseInside(mx, my, btnPlay)) {
@@ -107,16 +106,16 @@ void updatePauseMenu(App *app, Level *level, Player *player) {
     Uint32 mouseState = SDL_GetMouseState(&mouseX, &mouseY);
     bool isLeftClicked = (mouseState & SDL_BUTTON(SDL_BUTTON_LEFT));
 
-    if (!isLeftClicked)
+    if (!isLeftClicked) 
         app->mouseReleased = true;
-
-    int btnW = 300;
-    int btnH = 90;
+   
+    int btnW = 200;
+    int btnH = 60;
     int centerX = (WINDOW_WIDTH - btnW) / 2;
 
-    SDL_Rect btnPauseResume = { centerX, 250, btnW, btnH};
-    SDL_Rect btnPauseSettings = { centerX, 370, btnW, btnH };
-    SDL_Rect btnPauseExit = { centerX, 490, btnW, btnH };
+    SDL_Rect btnPauseResume     = { centerX, 250, btnW, btnH};
+    SDL_Rect btnPauseSettings = { centerX, 350, btnW, btnH };
+    SDL_Rect btnPauseExit     = { centerX, 450, btnW, btnH };
 
     if (isLeftClicked && app->mouseReleased) {
         if (isMouseInside(mouseX, mouseY, btnPauseResume)) {
@@ -140,13 +139,13 @@ void renderPauseMenu(App *app) {
     int mx, my;
     SDL_GetMouseState(&mx, &my);
 
-    int btnW = 300;
-    int btnH = 90;
+    int btnW = 200;
+    int btnH = 60;
     int centerX = (WINDOW_WIDTH - btnW) / 2;
 
-    SDL_Rect btnPauseResume = { centerX, 250, btnW, btnH};
-    SDL_Rect btnPauseSettings = { centerX, 370, btnW, btnH };
-    SDL_Rect btnPauseExit = { centerX, 490, btnW, btnH };
+    SDL_Rect btnPauseResume     = { centerX, 250, btnW, btnH};
+    SDL_Rect btnPauseSettings = { centerX, 350, btnW, btnH };
+    SDL_Rect btnPauseExit     = { centerX, 450, btnW, btnH };
 
     if (isMouseInside(mx, my, btnPauseResume)) SDL_SetRenderDrawColor(app->renderer, 100, 255, 100, 255);
     else SDL_SetRenderDrawColor(app->renderer, 50, 200, 50, 255);
@@ -161,23 +160,22 @@ void renderPauseMenu(App *app) {
     SDL_RenderFillRect(app->renderer, &btnPauseExit);
 }
 
-void updateGameOver(App *app, Level *level, Player *player, int CurrentLevel, SDL_Renderer *renderer) {
+void updateGameOver(App *app, Level *level,Player *player, int CurrentLevel, SDL_Renderer *renderer) {
     int mouseX, mouseY;
     SDL_GetMouseState(&mouseX, &mouseY);
     const Uint8 *keys = SDL_GetKeyboardState(NULL);
     Uint32 mouseState = SDL_GetMouseState(&mouseX, &mouseY);
-    bool isLeftClicked = (mouseState & SDL_BUTTON(SDL_BUTTON_LEFT));
-
-    if (!isLeftClicked)
+    bool isLeftClicked = (mouseState & SDL_BUTTON(SDL_BUTTON_LEFT));  
+    
+    if (!isLeftClicked) 
         app->mouseReleased = true;
-
+    
     int btnW = 200;
     int btnH = 60;
     int centerX = (WINDOW_WIDTH - btnW) / 2;
 
     SDL_Rect btnRestart = { centerX, 250, btnW, btnH };
     SDL_Rect btnQuit = { centerX, 340, btnW, btnH };
-
     if (keys[SDL_SCANCODE_RETURN] || (isLeftClicked && isMouseInside(mouseX, mouseY, btnRestart))) {
         initPlayer(player, level->spawnX, level->spawnY);
         initLevel(level, CurrentLevel);
@@ -185,6 +183,7 @@ void updateGameOver(App *app, Level *level, Player *player, int CurrentLevel, SD
         app->state = STATE_PLAY;
 
         app->gameStartTime = SDL_GetTicks();
+        
         if (app->currentLevel == 1 && app->level1Music) Mix_PlayMusic(app->level1Music, -1);
         else if (app->currentLevel == 2 && app->level2Music) Mix_PlayMusic(app->level2Music, -1);
         else if (app->currentLevel == 3 && app->level3Music) Mix_PlayMusic(app->level3Music, -1);
@@ -201,7 +200,7 @@ void updateGameOver(App *app, Level *level, Player *player, int CurrentLevel, SD
 void renderGameOver(App *app) {
     int mouseX, mouseY;
     SDL_GetMouseState(&mouseX, &mouseY);
-
+    
     int btnW = 200;
     int btnH = 60;
     int centerX = (WINDOW_WIDTH - btnW) / 2;
